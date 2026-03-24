2eee13a9e2a7b5afe0808028b58deb81b20a959e
MDVR544 <devriescool@gmail.com>
2026-03-24 16:57:30 +0100

    opdracht 1 done

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -7,15 +7,28 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 /* Opdracht  1: Cum Laude */
 
 /* 1a: Script schrijven  */
-// De administratie moet weten hoeveel studenten er dit blok cum laude zijn afgestudeerd (8 of hoger). Daar moeten namelijk speciale diploma's voor besteld worden.
+// // De administratie moet weten hoeveel studenten er dit blok cum laude zijn afgestudeerd (8 of hoger). Daar moeten namelijk speciale diploma's voor besteld worden.
 // Schrijf de stapjes om dit te kunnen checken eerst uit en vraag jezelf de volgende dingen af:
 // * Hoe kan ik iedere waarde van de array checken op deze conditie?
+    // door gebruik te maken van de for loop en
 // * Hoe zorg ik ervoor dat dit ook werkt wanneer de array 100 entries bevat?
+    //de counter op grades.length de zetten.
 // * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan bijhouden?
+    // door te loggen in een variabele
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6
 
+// let count = 0
+//
+//     for (let i = 0; i < grades.length; i++) {
+//
+//         if (grades[i] >= 8){
+//             count++;
+//         }
+// }
+//
+// console.log(count);
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
@@ -27,7 +40,21 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
+let count = 0
 
+function cumLaude(arr){
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] >= 8){
+            count++;
+        }
+    }
+    return console.log(count);
+}
+// cumLaude(grades);
+// cumLaude([6, 4, 5]);
+cumLaude([8, 9, 4, 6, 10]);
 
 
 /* Opdracht  2: Gemiddeld cijfer */
